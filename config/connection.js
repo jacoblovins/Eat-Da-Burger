ea7a5dece92972be2576ea1842d94c9eb858e24c
jacoblovins <lovins.jacob@yahoo.com>
2020-09-12 09:54:54 -0400

    jawsdb provision

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Setting up MySQL connection.
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "docker123",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "docker123",
+    database: "burgers_db"
+  });
+
+}
+
 
 // Make connection.
 connection.connect(err => {
